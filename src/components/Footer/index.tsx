86ed3f22fc23641f3de75223a6922c755f09dce5
narcoa47 <narcoa47@gmail.com>
2024-04-18 19:47:10 +0200

    changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -184,7 +184,7 @@ const Footer = () => {
                 <ul>
                   <li>
                     <Link
-                      href="/contact"
+                      href="/"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Partnerships
